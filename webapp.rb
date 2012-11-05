57a124592839e2d39063edca98bff9d2debc543e
Phil Pirozhkov <pirj@mail.ru>
2012-11-05 16:25:26 +0400

    make etc packages

--- webapp.rb
+++ webapp.rb
@@ -1,6 +1,7 @@
 dep 'web app', :app_path, :app_repo do
   requires [
     'imagemagick.managed',
+    'build-essential.managed',
     # 'nginx running',
     # 'postgres running',
     # ssh "sudo babushka pirj:postgres.managed version=9.1"
